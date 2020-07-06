6feb908e9dc154ead5ff2765fb579c2057f2caa8
Ingrid Grønlie Guren <ingrid.gronlie.guren@nrk.no>
2020-07-06 11:57:53 +0200

    Add specific text for my birthday 🥳

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -25,15 +25,30 @@ class Home extends PureComponent<IProps, IState>{
         const today = new Date();
         const numberOfDaysLeft = getNumberOfDaysBetweenDates(today, birthday);
 
+        let content = (
+            <div>
+                <p>... og jeg har bursdag om </p>
+                <h2 className="birthday-number" style={{color}}>{numberOfDaysLeft}</h2>
+                <p>dager <span role="img" aria-label="emoji star struck">🤩</span></p>
+            </div>
+        );
+
+        if (numberOfDaysLeft === 265 || numberOfDaysLeft === 0) {
+            content = (
+                <div>
+                    <p>... OG JEG HAR BURSDAG</p>
+                    <h2 className="birthday-number" style={{color}}> I DAG</h2>
+                </div>
+            )
+        }
+
         const { showConfetti } = this.state;
         return (
             <div className="home">
                 <h1 className="header-font">Hei! <span role="img" aria-label="waving emoji">👋</span></h1>
                 <p>Velkommen til min hjemmeside (og lekeplass)</p>
                 <div className="birthday">
-                    <p>... og jeg har bursdag om </p>
-                    <h2 className="birthday-number" style={{color}}>{numberOfDaysLeft}</h2>
-                    <p>dager <span role="img" aria-label="emoji star struck">🤩</span></p>
+                    {content}
                     <button
                         className="birthday-button"
                         style={{borderColor: color}}
