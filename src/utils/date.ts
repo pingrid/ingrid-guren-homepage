2de074b41358a5d5de331f0bbbd8eebca99145bb
Ingrid Grønlie Guren <ingrid.gronlie.guren@nrk.no>
2021-09-17 22:39:31 +0200

    Utils as ts

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -1,6 +1,6 @@
-export const getNumberOfDaysBetweenDates = (firstDate, secondDate) => {
+export function getNumberOfDaysBetweenDates (firstDate: Date, secondDate: Date): Number {
     if (firstDate.getTime() > secondDate.getTime()) {
         secondDate.setFullYear(secondDate.getFullYear() + 1);
     }
     return Math.ceil(Math.abs(secondDate.getTime() - firstDate.getTime()) / (24 * 60 * 60 * 1000));
-};
+}
